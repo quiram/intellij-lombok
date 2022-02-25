590ce9f74f39d35010884c15f9241e7c99280128
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2022-02-25 19:40:39 +0000

    split builder method calls into multiple lines

--- src/main/java/com/github/quiram/lombok/Application.java
+++ src/main/java/com/github/quiram/lombok/Application.java
@@ -2,7 +2,9 @@ package com.github.quiram.lombok;
 
 public class Application {
     public static void main(String[] args) {
-        final StringGroup stringGroup = StringGroup.builder().elements("abc", "123").build();
+        final StringGroup stringGroup = StringGroup.builder()
+                .elements("abc", "123")
+                .build();
         System.out.println(stringGroup);
     }
 }
